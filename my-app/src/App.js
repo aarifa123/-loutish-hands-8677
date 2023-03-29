9fa2e318e25c7c55864db782d9701e45d42631a5
aarifa123 <aarirashi786@gmail.com>
2023-03-29 21:33:48 +0530

    day1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './Fillthedata';
+function App() {
+  return (
+    <div className="App">
+      <MyForm/>
+     
+    </div>
+  );
+}
+
+export default App;
